989d9811660fb549b0c2391e9826987b83dbb40b
Jelmer Snoeck <jelmer@siphoc.com>
2018-01-02 11:54:50 +0100

    Teach our types about SecretType.
    This will allow users to define which type of secret they want generated
    from the credentials that get returned from their configuration.
    
    Currently, we'll only support Opaque (default) and Docker Config.

---
+++ primitives/utils.go
@@ -0,0 +1,19 @@
+package primitives
+
+import (
+	"fmt"
+
+	"k8s.io/api/core/v1"
+)
+
+func secretType(t string) (v1.SecretType, error) {
+	switch t {
+	case "opaque":
+	case "":
+		return v1.SecretTypeOpaque, nil
+	case "docker-registry":
+		return v1.SecretTypeDockercfg, nil
+	default:
+		return "", fmt.Errorf("Secret type '%s' not supported", t)
+	}
+}
